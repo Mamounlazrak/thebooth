bfa895ff87f8011c90c8062956aa768b87db6d7a
thatGui <gui@thatgui.com>
2022-02-21 10:41:35 +0000

    adde cloudinary + began work on user model

--- models/User.model.js
+++ models/User.model.js
@@ -3,11 +3,20 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
-      type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+    name: { type: String, required: true},
+    lastName: {type: String, required: true},
+    location: {City: String, required: true},
+    description: String,
+    avatar: { type: String,
+              default: 'images/default-avatar.png'
     },
-    password: String,
+    username: { type: String, 
+                required: true,
+                unique: true
+    },
+    password: { type: String, 
+      required: true
+    }
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
