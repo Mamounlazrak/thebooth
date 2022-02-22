9f8bb5cac9ed17ea3ee952f1e9aab65350ae4708
thatGui <gui@thatgui.com>
2022-02-22 15:33:37 +0000

    added api get route

--- routes/APIevents.js
+++ routes/APIevents.js
@@ -1,16 +1,23 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const User = require("../models/User.model");
+const axios = require('axios').default;
 
-router.get('/apievents', (req, res, next) => {
+
+router.get('/', (req, res, next) => {
     
-    axios.get('https://app.ticketmaster.com/discovery/v2/events.json?countryCode=PT&apikey=LuPNxYdSBVJ851VgC4AeG9J36vhAKV0X')
+    axios.get(`https://app.ticketmaster.com/discovery/v2/events?apikey=${process.env.TM_KEY}&locale=*&countryCode=PT`)
     .then(responseFromApi => {
-        console.log(responseFromApi)
-        /* res.render('/events/localEvents', responseFromApi) */
+        console.log(responseFromApi.data)
+        res.render('./events/localEvents.hbs', {data: responseFromApi.data})
     })
     .catch(error => console.log(error))
 
 })
 
 module.exports = router;
+
+//location: String,
+//${req.app.locals.user.location}
+// /discovery/v2/events /discovery/v2/classifications/genres/{Concerts}&apikey=${TM_KEY}
+// ${TM_KEY}
