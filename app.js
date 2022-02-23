1429ee2d928dc378f096077837414b8b2fa052c1
thatGui <gui@thatgui.com>
2022-02-23 15:40:53 +0000

    connections added

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(function(req, res, next) {
 const projectName = "thebooth";
 const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
+app.locals.title = `${capitalized(projectName)}`;
 // 👇 Start handling routes here
 const index = require("./routes/index");
 app.use("/", index);
@@ -48,6 +48,9 @@ app.use("/organic-event", orgEventsRoutes);
 
 const apiEvents = require("./routes/APIevents");
 app.use("/apievents", apiEvents);
+
+const connectionsRoutes = require("./routes/connections");
+app.use("/explore", connectionsRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- routes/Connections.js
+++ routes/Connections.js
@@ -3,8 +3,34 @@ const mongoose = require("mongoose");
 const User = require("../models/User.model");
 
 
-router.get('/', (req, res, next) => {
-    /* res.render('user/profile'); */
-})
+router.get("/", (req, res, next) => {
+    User.find().then( users => {
+       // console.log(users);
+        res.render('./connections/explore.hbs', {users});
+    })
+    .catch(error => console.log(error));
+});
+
+router.get("/profile/:id", (req, res, next) => {
+    const { id } = req.params;
+    User.findById(id).then( userinfo => {
+       // console.log(userinfo);
+        res.render('./connections/exploreProfile.hbs', userinfo);
+    })
+    .catch(error => console.log(error));
+});
+
+router.post("/profile/:id", (req, res, next) => {
+    const { id } = req.params;
+   //const {  } = req.body;
+   console.log(req.app.locals.user._id);
+   User.findByIdAndUpdate(req.app.locals.user._id, {$push : {connections: id}})
+    .then( userfollowed => {console.log(userfollowed); res.redirect('/');})
+    .catch(error => next(error));
+});
+  
 
 module.exports = router;
+
+
+// , {data: responseFromApi.data}
